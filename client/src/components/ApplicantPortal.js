2d572a8bf616b14e8f82e818ed79372e9b263923
seenso <>
2022-02-03 13:25:35 -0900

    Added modal for creating adoption application

--- client/src/components/ApplicantPortal.js
+++ client/src/components/ApplicantPortal.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Applications from "./Applications"
 import AdoptablePets from "./AdoptablePets";
 
 export default function ApplicantPortal({ currentUser, applications }) {
+  //filter applications for current user
+  // usersApps = applications.filter(app => app.applicant_id === currentUser.id);
   
   return (
     <div id="applicant_portal">
        <Switch>
         <Route exact path='/applicantportal'>
-          <Applications />
+          <Applications currentUser={currentUser}/>
         </Route>
         <Route path="/applicantportal/adoptablepets">
           <AdoptablePets />
